4302c3d3941ee3279d952264eef927fbfb40e6cb
Takeshi Miyakawa <tmiya@tmiya.dev>
2020-03-23 07:28:45 +0900

    a sample for closure

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,15 @@
 'use strict';
 
-console.log('Hello');
-
+window.onload = () => {
+  const text = document.getElementById('text0');
+  const button = document.getElementById('button0');
+  const report = document.getElementById('report');
+  button.onclick = () => {
+    console.dir(text);
+    const textValue = text.value;
+    const message = text.value ? 'Hello, ' + textValue : 'Hello, world!';
+    const p = document.createElement('p');
+    p.innerText = message;
+    report.appendChild(p);
+  };
+};
