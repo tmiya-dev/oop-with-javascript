fb507d4224cc7c704337ad149f649647e46a7d43
mariefujita <41368500+mariefujita@users.noreply.github.com>
2020-04-10 08:19:22 +0900

    index.js修正

--- src/js/index.js
+++ src/js/index.js
@@ -3,6 +3,16 @@
 function getPostalCode() {
   const postalCode = document.getElementById('postalCode').value;
   console.log(postalCode);
+  
+  const xhr = new XMLHttpRequest();
+  xhr.open('GET', 'http://zipcloud.ibsnet.co.jp/doc/api', true);
+  xhr.send();
+
+  xhr.onreadystatechange = function() {
+    if(xhr.readyState === 4) {
+      console.log('done');　
+    }
+  }
 }
 
 window.addEventListener('load', () => {
@@ -10,16 +20,6 @@ window.addEventListener('load', () => {
   const button = document.getElementById('getPostalCodeButton');
   console.dir(button);
   button.addEventListener('click', getPostalCode);
-
-  const xhr = new XMLHttpRequest();
-  xhr.open('GET', 'http://zipcloud.ibsnet.co.jp/doc/api', true);
-  xhr.send();
-
-  xhr.onreadystatechange = function() {
-    if(xhr.readyState === 4) {
-      console.log('done');
-    }
-  }
 });  
 
 
