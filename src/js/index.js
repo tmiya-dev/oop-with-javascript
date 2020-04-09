cdbf46532bd47a68461afd10bb17bcf9b9d9ee1a
mariefujita <41368500+mariefujita@users.noreply.github.com>
2020-04-10 08:00:00 +0900

    sample for zipcode api

--- src/js/index.js
+++ src/js/index.js
@@ -5,7 +5,25 @@ function getPostalCode() {
   console.log(postalCode);
 }
 
+window.addEventListener('load', () => {
+  console.log('loaded');
+  const button = document.getElementById('getPostalCodeButton');
+  console.dir(button);
+  button.addEventListener('click', getPostalCode);
 
-const xhr = new XMLHttpRequest();
+  const xhr = new XMLHttpRequest();
+  xhr.open('GET', 'http://zipcloud.ibsnet.co.jp/doc/api', true);
+  xhr.send();
 
-xhr.open()
+  xhr.onreadystatechange = function() {
+    if(xhr.readyState === 4) {
+      
+    }
+  }
+});  
+
+
+
+
+
+
