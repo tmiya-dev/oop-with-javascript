6516c9a5c2c8783cac4c2e170f207202e7dd56b5
mariefujita <41368500+mariefujita@users.noreply.github.com>
2020-04-13 07:22:09 +0900

    取得した値をinputに代入

--- src/js/index.js
+++ src/js/index.js
@@ -3,14 +3,21 @@
 function getPostalCode() {
   const postalCode = document.getElementById('postalCode').value;
   console.log(postalCode);
-  
+
   const xhr = new XMLHttpRequest();
-  xhr.open('GET', 'http://zipcloud.ibsnet.co.jp/doc/api', true);
+  xhr.open('GET', 'https://zip-cloud.appspot.com/api/search?zipcode=' + postalCode, true);
   xhr.send();
 
   xhr.onreadystatechange = function() {
     if(xhr.readyState === 4) {
       console.log('done');　
+      console.dir(xhr.response);
+      const obj = JSON.parse(xhr.response);
+      console.dir(obj);
+      const results = obj.results[0];
+      const address = results.address1 + results.address2 + results.address3;
+      console.log(address);
+      document.getElementById('inputAddress').value = address;
     }
   }
 }
