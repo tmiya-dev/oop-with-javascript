3b7a01e2b0244e157be958e5110ffd6c1cf0d1da
mariefujita <41368500+mariefujita@users.noreply.github.com>
2020-04-23 07:45:49 +0900

    配列の取得

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,26 @@
 'use strict';
 
-console.log('Hello');
+function onClickConvertButton() {
+  const text = document.getElementById('inputText');
+  const convertedResult = document.getElementById('convertedResult');
+  const encodedText= encodeURI(text.value);
+  console.dir(encodedText);
 
+  const xhr = new XMLHttpRequest();
+  xhr.open('GET', 'http://www.google.com/transliterate?langpair=ja-Hira|ja&text=' + encodedText, true);
+  xhr.send();
+
+  xhr.onreadystatechange = function() {
+    if(xhr.readyState === 4) {
+      const obj = JSON.parse(xhr.response);
+      console.dir(obj);
+      const results = obj.Array(1);
+      console.dir(results);
+    }
+  }
+}
+
+window.addEventListener('load', () => {
+  const button = document.getElementById('convertButton');
+  button.addEventListener('click', onClickConvertButton);
+});
