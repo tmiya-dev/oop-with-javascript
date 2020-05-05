facc04be1bb8755abb7ebc3fa7988a9880e18ca5
mariefujita <41368500+mariefujita@users.noreply.github.com>
2020-05-05 17:02:59 +0900

    encode text

--- src/js/index.js
+++ src/js/index.js
@@ -14,12 +14,15 @@ window.addEventListener('load', () => {
   for(let i = 0; i < buttons.length; ++i) {
     buttons[i].addEventListener('click', () => {
       unfinished += buttons[i].innerText
-      drawInputText(unfinished)
+      drawInputText(unfinished);
+      const encodedText = encodeURI(unfinished);
+      console.dir(encodedText);
       const xhr = new XMLHttpRequest();
       xhr.open('GET', 'http://www.google.com/transliterate?langpair=ja-Hira|ja&text=' + unfinished, true);
       xhr.send();
       xhr.onreadystatechange = function() {
         if(xhr.readyState === 4) {
+          const respond = JSON.parse()
           console.log('connected');
         }
       } 
