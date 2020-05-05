14f332d47a8b3dd2da86c3e1b6ad81eb9fb497a6
Takeshi Miyakawa <tmiya@tmiya.dev>
2020-05-05 16:44:13 +0900

    update: index.js
    キーをタップしたらinputTextに挿入

--- src/js/index.js
+++ src/js/index.js
@@ -2,14 +2,20 @@ import '../scss/style.scss';
 
 'use strict';
 
-function onClickKeyboardButton() {
+function drawInputText(unfinished) {
+  const field = document.getElementById('inputText');
+  field.innerText = unfinished
 }
+
+
+let unfinished = ''
 
 window.addEventListener('load', () => {
   const buttons = document.getElementsByClassName('keyboard-button');
   for(let i = 0; i < buttons.length; ++i) {
     buttons[i].addEventListener('click', () => {
-      alert(buttons[i].innerText);
-    }) 
+      unfinished += buttons[i].innerText
+      drawInputText(unfinished)
+    })
   }
 });
