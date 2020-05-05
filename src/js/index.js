8f0e45935ed3d4d9f08a9e8d9db8c3c70501f564
mariefujita <41368500+mariefujita@users.noreply.github.com>
2020-05-05 16:56:37 +0900

    connect to api

--- src/js/index.js
+++ src/js/index.js
@@ -7,7 +7,6 @@ function drawInputText(unfinished) {
   field.innerText = unfinished
 }
 
-
 let unfinished = ''
 
 window.addEventListener('load', () => {
@@ -16,6 +15,14 @@ window.addEventListener('load', () => {
     buttons[i].addEventListener('click', () => {
       unfinished += buttons[i].innerText
       drawInputText(unfinished)
+      const xhr = new XMLHttpRequest();
+      xhr.open('GET', 'http://www.google.com/transliterate?langpair=ja-Hira|ja&text=' + unfinished, true);
+      xhr.send();
+      xhr.onreadystatechange = function() {
+        if(xhr.readyState === 4) {
+          console.log('connected');
+        }
+      } 
     })
   }
 });
