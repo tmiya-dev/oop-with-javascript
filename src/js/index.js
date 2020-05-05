053fccdc1d57a4b6bd3ba7dea0c2ae1a6c67cdfe
mariefujita <41368500+mariefujita@users.noreply.github.com>
2020-05-05 16:28:47 +0900

    get event

--- src/js/index.js
+++ src/js/index.js
@@ -3,13 +3,9 @@ import '../scss/style.scss';
 'use strict';
 
 function onClickKeyboardButton() {
-  const button = document.getElementById('keyboard').innerHTML;
-  const text = document.getElementById('inputText');
 }
 
 window.addEventListener('load', () => {
-  const keyboardButton = document.getElementById('keyboard');
-  for(let i = 0; i < keyboardButton.length; i++) {
-    keyboardButton.addEventListener('click', onClickKeyboardButton);
-  }
+  const buttons = document.getElementsByClassName('keyboard-button');
+  console.dir(buttons);
 });
