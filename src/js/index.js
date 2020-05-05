b3e0055b760eb673e2a70e0459888b637d52e5bf
Takeshi Miyakawa <tmiya@tmiya.dev>
2020-05-05 17:09:52 +0900

    refactor

--- src/js/index.js
+++ src/js/index.js
@@ -4,28 +4,33 @@ import '../scss/style.scss';
 
 function drawInputText(unfinished) {
   const field = document.getElementById('inputText');
-  field.innerText = unfinished
+  field.innerText = unfinished;
 }
 
-let unfinished = ''
+let unfinished = '';
+
+function onClickButton(button) {
+  unfinished += button.innerText
+  drawInputText(unfinished);
+  const encodedText = encodeURI(unfinished);
+  console.dir(encodedText);
+  const xhr = new XMLHttpRequest();
+  xhr.open('GET', 'http://www.google.com/transliterate?langpair=ja-Hira|ja&text=' + encodedText, true);
+  xhr.send();
+  xhr.onreadystatechange = function() {
+    if(xhr.readyState === 4) {
+      const respond = JSON.parse(xhr.response);
+      console.dir(respond);
+    }
+  }
+}
+
 
 window.addEventListener('load', () => {
   const buttons = document.getElementsByClassName('keyboard-button');
   for(let i = 0; i < buttons.length; ++i) {
     buttons[i].addEventListener('click', () => {
-      unfinished += buttons[i].innerText
-      drawInputText(unfinished);
-      const encodedText = encodeURI(unfinished);
-      console.dir(encodedText);
-      const xhr = new XMLHttpRequest();
-      xhr.open('GET', 'http://www.google.com/transliterate?langpair=ja-Hira|ja&text=' + encodedText, true);
-      xhr.send();
-      xhr.onreadystatechange = function() {
-        if(xhr.readyState === 4) {
-          const respond = JSON.parse(xhr.response);
-          console.dir(respond);
-        }
-      } 
+      onClickButton(buttons[i])
     })
   }
 });
