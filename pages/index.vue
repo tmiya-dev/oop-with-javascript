1ba7824f35e8e464e4ce15ae98fd7657856446b6
mariefujita <41368500+mariefujita@users.noreply.github.com>
2020-05-06 13:16:18 +0900

    up index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,23 @@
 <template>
-  <h1>Hello World</h1>
+  <div class="main-content">
+    <div id="input-field" class="input-text"></div>
+    <div id="conversion-results"></div>
+    <div id="keyboard"></div>
+  </div>
 </template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style lang="sass">
+
+.main-content {
+  width: 80%;
+}
+.input-text {
+  border: 1px solid #ddd;
+}
+</style>
