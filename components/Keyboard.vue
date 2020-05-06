1f4e4bce2729004648a93ea39cb14bb92bab8201
Takeshi Miyakawa <tmiya@tmiya.dev>
2020-05-06 13:58:36 +0900

    add: Keyboard.vue

---
+++ components/Keyboard.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <dl v-for="row of keys">
+            <dd v-for="key of row">
+                {{key}}
+            </dd>
+        </dl>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Keyboard',
+    props: {
+        keys: {
+            type: Array,
+            required: true
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -2,12 +2,29 @@
   <div class="main-content">
     <div id="input-field" class="input-text"></div>
     <div id="conversion-results"></div>
-    <div id="keyboard"></div>
+    <div id="keyboard">
+      <Keyboard
+        :keys="keys"
+      >
+      </Keyboard>
+    </div>
   </div>
 </template>
 
 <script>
+import Keyboard from '../components/Keyboard';
 export default {
+  components: {
+    Keyboard
+  },
+  data () {
+    return {
+      keys: [
+        'あいうえお'.split(''),
+        'かきくけこ'.split(''),
+      ]
+    }
+  }
   
 }
 </script>
